90d6935acdaea8165cceec814f327087ec323b23
Azul <101115227+azulmart1234@users.noreply.github.com>
2022-04-11 10:11:16 -0300

    Add files via upload

---
+++ Simulacion de BD (1).sql
@@ -0,0 +1,163 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-04-2022 a las 15:07:10
+-- Versión del servidor: 10.4.20-MariaDB
+-- Versión de PHP: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `empresa-martinetto y rassi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `articulos`
+--
+
+CREATE TABLE `articulos` (
+  `T#` varchar(30) NOT NULL,
+  `TNOMBRE` varchar(30) NOT NULL,
+  `CIUDAD` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `articulos`
+--
+
+INSERT INTO `articulos` (`T#`, `TNOMBRE`, `CIUDAD`) VALUES
+('T1', 'CLASIFICADORA', 'MADRID'),
+('T2', 'PERFORADORA', 'MALAGA'),
+('T3', 'LECTORA', 'CACERES'),
+('T4', 'CONSOLA', 'CACERES'),
+('T5', 'MEZCLADORA', 'SEVILLA'),
+('T6', 'TERMINAL', 'BARCELONA');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `componentes`
+--
+
+CREATE TABLE `componentes` (
+  `C#` varchar(30) NOT NULL,
+  `CNOMBRE` varchar(30) NOT NULL,
+  `COLOR` varchar(30) NOT NULL,
+  `PESO` double NOT NULL,
+  `CIUDAD` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `componentes`
+--
+
+INSERT INTO `componentes` (`C#`, `CNOMBRE`, `COLOR`, `PESO`, `CIUDAD`) VALUES
+('C1', 'X3A', 'ROJO', 12, 'SEVILLA'),
+('C3', 'C4B', 'AZUL', 17, 'MALAGA'),
+('C4', 'C4B', 'ROJO', 14, 'SEVILLA'),
+('C5', 'VT8', 'AZUL', 12, 'MADRID'),
+('C6', 'C30', 'ROJO', 19, 'SEVILLA');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `envios`
+--
+
+CREATE TABLE `envios` (
+  `P#` varchar(30) NOT NULL,
+  `C#` varchar(30) NOT NULL,
+  `T#` varchar(30) NOT NULL,
+  `CANTIDAD` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `envios`
+--
+
+INSERT INTO `envios` (`P#`, `C#`, `T#`, `CANTIDAD`) VALUES
+('P1', 'C1', 'T1', 200),
+('P2', 'C3', 'T1', 400),
+('P2', 'C3', 'T2', 200),
+('P2', 'C3', 'T3', 200),
+('P2', 'C3', 'T4', 500),
+('P2', 'C3', 'T5', 600),
+('P2', 'C3', 'T6', 400),
+('P2', 'C3', 'T7', 800),
+('P2', 'C5', 'T2', 100),
+('P3', 'C3', 'T1', 200),
+('P3', 'C4', 'T2', 500),
+('P4', 'C6', 'T3', 300),
+('P4', 'C6', 'T7', 300),
+('P5', 'C2', 'T2', 200),
+('P5', 'C2', 'T4', 100),
+('P5', 'C5', 'T4', 500),
+('P5', 'C5', 'T7', 100),
+('P5', 'C6', 'T2', 200),
+('P5', 'C1', 'T4', 100),
+('P5', 'C3', 'T4', 200),
+('P5', 'C4', 'T4', 800),
+('P5', 'C5', 'T5', 400),
+('P5', 'C6', 'T4', 500);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `provedores`
+--
+
+CREATE TABLE `provedores` (
+  `P#` varchar(30) NOT NULL,
+  `PNOMBRE` varchar(30) NOT NULL,
+  `CATEGORIA` double NOT NULL,
+  `CIUDAD` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `provedores`
+--
+
+INSERT INTO `provedores` (`P#`, `PNOMBRE`, `CATEGORIA`, `CIUDAD`) VALUES
+('P1', 'CARLOS', 20, 'SEVILLA'),
+('P2', 'JUAN', 10, 'MADRID'),
+('P3', 'JOSE', 30, 'SEVILLA'),
+('P4', 'INMA', 20, 'SEVILLA');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `articulos`
+--
+ALTER TABLE `articulos`
+  ADD PRIMARY KEY (`T#`);
+
+--
+-- Indices de la tabla `componentes`
+--
+ALTER TABLE `componentes`
+  ADD PRIMARY KEY (`C#`);
+
+--
+-- Indices de la tabla `provedores`
+--
+ALTER TABLE `provedores`
+  ADD PRIMARY KEY (`P#`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
